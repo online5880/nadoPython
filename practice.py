33f910d0fe8fd5fcc95b205f9b2ffd3df4033c61
online5880 <gjtjqkr1@naver.com>
2024-06-27 20:39:37 +0900

    변수

--- practice.py
+++ practice.py
@@ -1,8 +1,13 @@
-﻿# 참 / 거짓
-print(5 > 10)
-print(5 < 10)
-print(True)
-print(False)
-print(not True)
-print(not False)
-print(not (5 > 10))
+﻿# 애완동물을 소개해 주세요!
+animal = "강아지"
+name = "연탄이"
+age = 4
+hobby = "산책"
+is_adult = age >= 3
+
+
+print("우리집 "+animal+" 이름은" +name+"이예요")
+hobby = "공놀이"
+#print(name+"는 "+str(age)+"살이며, "+hobby+"을 아주 좋아해요")
+print(name,"는",age,"살이며, ",hobby,"을 아주 좋아해요")
+print(name+"는 어른일까요? "+str(is_adult))
