472545dbc33eefbb21c1b9e9e5717c78d66b8b5e
online5880 <gjtjqkr1@naver.com>
2024-06-27 20:49:49 +0900

    주석

--- practice.py
+++ practice.py
@@ -5,9 +5,12 @@ age = 4
 hobby = "산책"
 is_adult = age >= 3
 
+'''이렇게 하면
+주석처리가
+됩니다...
+'''
 
 print("우리집 "+animal+" 이름은" +name+"이예요")
 hobby = "공놀이"
-#print(name+"는 "+str(age)+"살이며, "+hobby+"을 아주 좋아해요")
 print(name,"는",age,"살이며, ",hobby,"을 아주 좋아해요")
 print(name+"는 어른일까요? "+str(is_adult))
