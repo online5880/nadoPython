5847d039b0859531ab07c56ce2c4b7dc96fed9ea
online5880 <gjtjqkr1@naver.com>
2024-07-01 01:15:44 +0900

    연산자

--- practice.py
+++ practice.py
@@ -1,8 +1,32 @@
-﻿#Quiz) 변수를 이용하여 다음 문장을 출력하시오
+﻿print(1+1) # 2
+print(2-1) # 1
+print(2*2) # 4
+print(3/2) # 1.5
 
-#변수명: station
-#변수값: "사당", "신도림", "인천공항" 순서대로 입력
-#출력문장: XX행 열차가 들어오고 있습니다.
+print(2**3) # 2^3 = 8
+print(5%3) # 5/3 = 1 ... 2
+print(10%3) # 10/3 = 3 ... 1
+print(5//3) # 5/3 = 1.666666, 1
+print(10//3) # 10/3 = 3.333333, 3
 
-station = "사당"
-print(station + "행 열차가 들어오고 있습니다.")
+print(10 > 3) # True
+print(4 >= 7) # False
+print(10 < 3) # False
+print(5 <= 5) # True
+
+print(3 == 3) # True
+print(4 == 2) # False
+print(3 + 4 == 7) # True
+
+print(1 != 3) # True
+print(not(1 != 3)) # False
+
+print((3 > 0) and (3 < 5)) # True
+print((3 > 0) & (3 < 5)) # True
+
+print((3 > 0) or (3 > 5)) # True
+print((3 > 0) | (3 > 5)) # True
+
+print(5 > 4 > 3) # True
+print(5 > 4 > 7) # False
+
