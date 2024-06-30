ffb1ab1c18c9deaf2944c0cce185bfd4eecc9d77
online5880 <gjtjqkr1@naver.com>
2024-07-01 01:16:40 +0900

    간단한 수식

--- practice.py
+++ practice.py
@@ -1,32 +1,22 @@
-﻿print(1+1) # 2
-print(2-1) # 1
-print(2*2) # 4
-print(3/2) # 1.5
+﻿print(2+3*4) # 14
+print((2+3)*4) # 20
+number = 2+3*4 # 14
+print(number)
 
-print(2**3) # 2^3 = 8
-print(5%3) # 5/3 = 1 ... 2
-print(10%3) # 10/3 = 3 ... 1
-print(5//3) # 5/3 = 1.666666, 1
-print(10//3) # 10/3 = 3.333333, 3
+number = number + 2 # 16
+print(number)
 
-print(10 > 3) # True
-print(4 >= 7) # False
-print(10 < 3) # False
-print(5 <= 5) # True
+number += 2 # 18
+print(number)
 
-print(3 == 3) # True
-print(4 == 2) # False
-print(3 + 4 == 7) # True
+number *= 2 # 36
+print(number)
 
-print(1 != 3) # True
-print(not(1 != 3)) # False
+number /= 2 # 18
+print(number)
 
-print((3 > 0) and (3 < 5)) # True
-print((3 > 0) & (3 < 5)) # True
+number -= 2 # 16
+print(number)
 
-print((3 > 0) or (3 > 5)) # True
-print((3 > 0) | (3 > 5)) # True
-
-print(5 > 4 > 3) # True
-print(5 > 4 > 7) # False
-
+number %= 5 # 1
+print(number)
