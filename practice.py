5918bdbf6a2776627f793baee3bbea04810cb18c
online5880 <gjtjqkr1@naver.com>
2024-07-04 20:41:55 +0900

    슬라이싱

--- practice.py
+++ practice.py
@@ -1,7 +1,10 @@
-﻿sentence = '나는 소년입니다.'
-print(sentence)
-sentence2 = '파이썬은 쉬워요'
-print(sentence2)
-sentence3 = """
-나는 소년이고, 파이썬은 쉬워요"""
-print(sentence3)
+﻿jumin = '990120-1234567'
+
+print('성별 : ' + jumin[7])
+print('연 : ' + jumin[0:2])
+print('월 : '+ jumin[2:4])
+print('일 : '+ jumin[4:6])
+
+print('생년월일 : '+ jumin[:6]) # 처음부터 6 직전까지 
+print('뒤 7자리 : '+jumin[7:])
+print('뒤 7자리 (뒤에서부터) : '+jumin[-7:])
