0e45aef2050e729f4ce29d26d8d7e059fa898728
online5880 <gjtjqkr1@naver.com>
2024-07-04 20:35:18 +0900

    문자열

--- practice.py
+++ practice.py
@@ -1,36 +1,7 @@
-﻿from random import *
-
-# 랜덤
-# print(random()) # 0.0 ~ 1.0 미만의 임의의 값 생성
-# print(random() * 10) # 0.0 ~ 10.0 미만의 임임의 값 생성
-# print(int(random() * 10)) # 0 부터 10 미만의 임임의 값 생성
-# print(int(random() * 10)) # 0 부터 10 미만의 임임의 값 생성
-# print(int(random() * 10)) # 0 부터 10 미만의 임임의 값 생성
-# print(int(random() * 10) + 1) # 1 부터 10 이하의 임임의 값 생성
-# print(int(random() * 10) + 1) # 1 부터 10 이하의 임임의 값 생성
-# print(int(random() * 10) + 1) # 1 부터 10 이하의 임임의 값 생성
-# print(int(random() * 10) + 1) # 1 부터 10 이하의 임임의 값 생성
-# print(int(random() * 10) + 1) # 1 부터 10 이하의 임임의 값 생성
-# print(int(random() * 10) + 1) # 1 부터 10 이하의 임임의 값 생성
-# print(int(random() * 10) + 1) # 1 부터 10 이하의 임임의 값 생성
-
-# print(int(random() * 45) + 1) # 1 부터 45 이하의 임임의 값 생성
-# print(int(random() * 45) + 1) # 1 부터 45 이하의 임임의 값 생성
-# print(int(random() * 45) + 1) # 1 부터 45 이하의 임임의 값 생성
-# print(int(random() * 45) + 1) # 1 부터 45 이하의 임임의 값 생성
-# print(int(random() * 45) + 1) # 1 부터 45 이하의 임임의 값 생성
-# print(int(random() * 45) + 1) # 1 부터 45 이하의 임임의 값 생성
-
-# print(randrange(1,46)) # 1 부터 45 미만의 임임의 값 생성
-# print(randrange(1,46)) # 1 부터 45 미만의 임임의 값 생성
-# print(randrange(1,46)) # 1 부터 45 미만의 임임의 값 생성
-# print(randrange(1,46)) # 1 부터 45 미만의 임임의 값 생성
-# print(randrange(1,46)) # 1 부터 45 미만의 임임의 값 생성
-# print(randrange(1,46)) # 1 부터 45 미만의 임임의 값 생성
-
-print(randint(1, 45)) # 1 부터 45 이하의 임임의 값 생성
-print(randint(1, 45)) # 1 부터 45 이하의 임임의 값 생성
-print(randint(1, 45)) # 1 부터 45 이하의 임임의 값 생성
-print(randint(1, 45)) # 1 부터 45 이하의 임임의 값 생성
-print(randint(1, 45)) # 1 부터 45 이하의 임임의 값 생성
-print(randint(1, 45)) # 1 부터 45 이하의 임임의 값 생성
+﻿sentence = '나는 소년입니다.'
+print(sentence)
+sentence2 = '파이썬은 쉬워요'
+print(sentence2)
+sentence3 = """
+나는 소년이고, 파이썬은 쉬워요"""
+print(sentence3)
