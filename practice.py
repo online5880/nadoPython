b11118cf25ff2411cce0d4b296b6b38dc5cd21f8
online5880 <gjtjqkr1@naver.com>
2024-07-05 00:23:03 +0900

    탈출 문자

--- practice.py
+++ practice.py
@@ -1,24 +1,20 @@
-﻿# 문자열 포맷
+﻿# \n : 줄바꿈
+print("백문이 불여일견\n백견이 불여일타")
 
-# 방법  1
-# print("나는 %d살입니다." % 20)
-# print("나는 %s을 좋아해요" % "파이썬")
-# print("Apple 은 %c로 시작해요." % "A")
-# %s
-# print("나는 %s살입니다." % 20)
-# print("나는 %s색과 %s색을 좋아해요." % ("파란", "빨간"))
+# \" \' : 문장 내에서 따옴표
+# 저는 "나도코딩"입니다.
+# print('저는 "나도코딩"입니다.')
+print("저는 \"나도코딩\"입니다.")
+print("저는 \'나도코딩\'입니다.")
 
-# 방법 2
-# print("나는 {}살입니다.".format(20))
-# print("나는 {}색과 {}색을 좋아해요".format("파란","빨간"))
-# print("나는 {0}색과 {1}색을 좋아해요".format("파란","빨간"))
-# print("나는 {1}색과 {0}색을 좋아해요".format("파란","빨간"))
+# \\ : 문장 내에서는 \ 의 역슬래시로
+print("F:\\강의\\나도Python\\pythonProject\\")
 
-# 방법 3
-# print("나는 {age}살이며, {color}색을 좋아해요".format(age = 20, color="빨간"))
-# print("나는 {age}살이며, {color}색을 좋아해요".format(color="빨간",age = 20))
+# \r : 커서를 맨 앞으로 이동
+print("Red Apple\rPine")
 
-# 방법 4
-age = 20
-color = "빨간"
-print(f"나는 {age}살이며, {color}색을 좋아해요")
+# \b : 백스페이스 ( 한 글자 삭제 )
+print("Redd\bApple")
+
+# \t : 탭
+print("Red\tApple")
