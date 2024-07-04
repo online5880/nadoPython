66708dc317bd973a9606a5b0fc33f822e8782ba1
online5880 <gjtjqkr1@naver.com>
2024-07-04 20:25:19 +0900

    숫자처리 함수

--- practice.py
+++ practice.py
@@ -20,3 +20,28 @@ print(number)
 
 number %= 5 # 1
 print(number)
+
+# 절대값
+print(abs(-5)) #5
+
+# 제곱
+print(pow(4,2)) # 4*4 = 16
+
+# 최소, 최대
+print(min(5, 12)) # 5
+print(max(5, 12)) # 12
+
+# 반올림
+print(round(3.14)) # 3
+print(round(4.99)) # 5
+
+
+from math import *
+# 내림
+print(floor(4.99)) # 4
+
+# 올림
+print(ceil(3.14)) # 4
+
+# 제곱근
+print(sqrt(16)) # 4
