08f32f85c39c6e731c07e336e1ad52cb024f5c72
online5880 <gjtjqkr1@naver.com>
2024-07-07 22:30:16 +0900

    if

--- practice.py
+++ practice.py
@@ -1,23 +1,18 @@
-﻿# 댓글 이벤트
-# 댓글 작성자들 중에 추첨을 통해 1명은 치킨, 3명은 커피 쿠폰을 받게 됩니다.
+﻿# weather = input("오늘 날씨는 어때요?")
+# if weather == "비":
+#     print("우산을 챙기세요")
+# elif weather == "미세먼지":
+#     print("마스크를 챙기세요")
+# else:
+#     print("오늘은 준비물이 필요 없어요")
 
-# 조건 1 : 편의상 댓글은 20명이 작성하였고 아이디는 1~20 이라고 가정
-# 조건 2 : 댓글 내용과 상관 없이 무작위로 추첨하되 중복 불가
-# 조건 3 : random 모듈의 suffle 과 sample 을 활용
-
-# 출력 예제
-# -- 당첨자 발표--
-# 치킨 당첨자 : 1
-# 커피 담청자 : [2, 3, 4]
-# -- 축하합니다 --
-
-from random import *
-students = list(range(1,21))
-
-shuffle(students)
-
-print('-- 당첨자 발표 --')
-winners = sample(students,4)
-print('치킨 당첨자 : {}'.format(winners[0]))
-print('커피 당첨자 : {}'.format(winners[1:]))
-print('-- 축하합니다 --')
+temp = int(input("기온은 어때요? "))
+if 30 <= temp:
+    print("너무 더워요. 나가지 마세요")
+elif 10 <= temp < 30:
+    print("괜찮은 날씨에요")
+elif 0 <= temp < 10:
+    print("외투를 챙기세요")
+else:
+    print("너무 추워요. 나가지 마세요")
+    
