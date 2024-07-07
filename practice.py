430fff701897e000521e8addbf55aeae50f5bda2
online5880 <gjtjqkr1@naver.com>
2024-07-07 21:59:56 +0900

    자료구조의 변경

--- practice.py
+++ practice.py
@@ -1,28 +1,13 @@
-﻿# 집합 (set)
-# 중복 안됨. 순서 없음
+﻿# 자료구조의 변경
+# 커피숍
+menu = {"커피", "우유", "주스"}
+print(menu, type(menu))
 
-my_set = {1,2,3,3,3}
-print(my_set)
+menu = list(menu)
+print(menu, type(menu))
 
-java = {"유재석","김태호","양세형"}
-python = set(["유재석","박명수"])
+menu = tuple(menu)
+print(menu, type(menu))
 
-# 교집합 (java 와 python 을 모두 할 수 있는 개발자
-print(java & python)
-print(java.intersection(python))
-
-# 합집함 (java 할 수 있거나 python 할 수 있는 개발자)
-print(java | python)
-print(java.union(python))
-
-# 차집합 (java 할 수 있지만 python 은 할 줄 모르는 개발자
-print(java - python)
-print(java.difference(python))
-
-# python 할 줄 아는 사람이 늘어남
-python.add("김태호")
-print(python)
-
-# java 를 잊었어요
-java.remove("김태호")
-print(java)
+menu = set(menu)
+print(menu, type(menu))
