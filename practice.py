ea37030d8757570e7e2289dbf76d6af8bc49e342
online5880 <gjtjqkr1@naver.com>
2024-07-08 21:23:02 +0900

    한 줄 for

--- practice.py
+++ practice.py
@@ -1,9 +1,17 @@
-﻿absent = [2, 5] # 결석
-no_book = [7]
-for student in range(1, 11):
-    if student in absent:
-        continue
-    elif student in no_book:
-        print("오늘 수업 여기까지. {}는 교무실로 따라와".format(student))
-        break    
-    print("{}, 책을 읽어봐".format(student))
+﻿# 한 줄 for
+# 출석번호가 1 2 3 4, 앞에 100을 붙이기로 함 -> 101, 102 , 103 , 104
+students = [1,2,3,4,5]
+print(students)
+students = [i+100 for i in students]
+print(students)
+
+# 학생 이름을 길이로 변환
+students = ["Iron man", "Thor", "I am groot"]
+print(students)
+students = [len(i) for i in students]
+print(students)
+
+# 학생 이름을 대문자로 변환
+students = ["Iron man", "Thor", "I am groot"]
+students = [i.upper() for i in students]
+print(students)
