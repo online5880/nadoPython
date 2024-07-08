c2253dafffe91a903d5f5d8573bfb95557fa7ea7
online5880 <gjtjqkr1@naver.com>
2024-07-08 21:15:17 +0900

    while

--- practice.py
+++ practice.py
@@ -1,9 +1,21 @@
-﻿for waiting_no in [0,1,2,3,4]:
-    print("대기번호 : {}".format(waiting_no))
+﻿# # while
+# customer = "토르"
+# Index = 5
+# while Index >= 1:
+#     print("{}, 커피가 준비되었습니다. {} 번 남았어요".format(customer,Index))
+#     Index -= 1
+#     if Index == 0:
+#         print("커피는 폐기처분되었습니다.")
 
-for waiting_no in range(5):
-    print("대기번호 : {}".format(waiting_no))
-    
-starbucks = ["아이언맨","토르","아이엠 그루트"]
-for customer in starbucks:
-    print("{0}, 커피가 준비되었습니다.".format(customer))
+# customer = "아이언맨"
+# Index = 1
+# while True:
+#     print("{}, 커피가 준비되었습니다.호출 {} 회".format(customer,Index))
+#     Index += 1
+
+customer = "토르"
+person = "Unknown"
+
+while person != customer:
+    print("{}, 커피가 준비되었습니다.".format(customer))
+    person = input("이름이 어떻게 되세요? ")
