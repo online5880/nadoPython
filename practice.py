871aa1675aa26f6be7971e6fb5389fc3cb264eef
online5880 <gjtjqkr1@naver.com>
2024-07-09 22:24:13 +0900

    기본값

--- practice.py
+++ practice.py
@@ -1,27 +1,14 @@
-﻿# 함수
+﻿# 기본값 default parameter
 
-def open_account():
-    print("새로운 계좌가 생성되었습니다.")
-    
-def deposit(balance, money): # 입금
-    print("입금이 완려되었습니다. 잔액은 {} 원입니다.".format(balance+money))
-    return balance + money
+# def profile(name, age, main_lang):
+#     print(f"이름은 : {name}\t나이 : {age}\t주 사용 언어 : {main_lang}")
+#     
+# profile("유재석",20,"파이썬")
+# profile("김태호",25,"자바")
 
-def withdraw(balance, money): # 출금
-    if balance >= money: # 잔액이 출금보다 많으면
-        print("출금이 완료되었습니다. 잔액은 {} 원입니다.".format(balance-money))
-        return balance - money
-    else:
-        print("출금이 완료되지 않았습니다. 잔액은 {} 원입니다.".format(balance))
-        return balance
-    
-def withdraw_night(balance, money): # 저녁에 출금
-    commission = 100 # 수수료 100원
-    return commission, balance - money - commission
+# 같은 학교 같은 학년 같은 반 같은 수업
+def profile(name, age=17, main_lang="파이썬"):
+    print(f"이름은 : {name}\t나이 : {age}\t주 사용 언어 : {main_lang}")
 
-balance = 0
-balance = deposit(balance, 1000)
-balance = withdraw(balance, 2000)
-# balance = withdraw(balance, 500)
-commission, balance = withdraw_night(balance, 500)
-print("수수료는 {} 원이며, 잔액은 {} 원입니다.".format(commission, balance))
+profile("유재석")
+profile("김태호")
