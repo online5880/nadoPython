a325138f81b412d1c6bdc11f7b3e0f81c8a843db
online5880 <gjtjqkr1@naver.com>
2024-07-10 18:10:18 +0900

    Quiz #5

--- practice.py
+++ practice.py
@@ -1,20 +1,19 @@
-﻿# 지역변수와 전역 변수
+﻿# Quiz ) 당신은 Cocoa 서비스를 이용하는 택시 기사이빈다.
+# 50 명의 승객과 매칭 기회가 있을 때, 총 탑승 승객 수를 구하는 프로그램을 작성하시오
 
-gun = 10
+# 조건 1 : 승객별 운행 소요 시간은 5분 ~ 50분 사이의 난수로 정해집니다.
+# 조건 2 : 당신은 소요 시간 5분 ~ 15분 사이의 승객만 매칭해야 합니다.
 
+import random
 
-def checkpoint(soldiers):  # 경계근무
-    global gun  # 전역 공간에 있는 gun 사용
-    gun = gun - soldiers
-    print("[함수 내] 남은 총 : {}".format(gun))
-
-
-def checkpoint_ret(gun, soldiers):
-    gun = gun - soldiers
-    print("[함수 내] 남은 총 : {}".format(gun))
-    return gun
-
-print("전체 총 : {}".format(gun))
-# checkpoint(2)
-gun = checkpoint_ret(gun, 2)
-print("남은 총 : {}".format(gun))
+person = 0
+for i in range(1, 51):
+    time = random.randint(5, 50)
+    if 5 <= time <= 15:
+        print("[O] {}번째 손님 (소요시간 : {}분)".format(i,time))
+        person+=1
+    else:
+        print("[ ] {}번째 손님 (소요시간 : {}분)".format(i,time))
+        
+print("총 탑승 승객 : {} 분".format(person))
+        
